8e51f9b5f16e04abb30103241c8679b8a2172279
mokautsar30 <mohammadkautsar30@gmail.com>
2024-01-17 00:18:50 +0700

    feat: login page layouting

--- client-web/src/App.jsx
+++ client-web/src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center text-white">
-        Hello world!
+      <h1 className="text-3xl font-bold underline text-center text-black">
+      <HomePage/>
       </h1>
-      <Outlet />
     </>
   );
 }
--- client-web/src/pages/HomePage.jsx
+++ client-web/src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
-export default function HomePage() {
-  return <div>INI HOME PAGE</div>;
-}
+import React from "react";
+import Navbar from "../components/navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-10">ini konten homepage</div>
+    </div>
+  );
+};
+
+export default HomePage;
