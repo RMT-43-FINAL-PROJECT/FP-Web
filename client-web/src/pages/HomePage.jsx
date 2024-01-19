be86f4205878063e756cee131135a3a08722fdc2
mokautsar30 <mohammadkautsar30@gmail.com>
2024-01-19 11:56:10 +0700

    feat: home chart prototype

--- client-web/src/pages/HomePage.jsx
+++ client-web/src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Navbar from "../components/navbar";
+// import Navbar from "../components/navbar";
+
 import Sidebar from "../components/Sidebar";
 import HomeCard from "../components/HomeCard";
+import Navbar from "../components/navbar/Navbar";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <div className="flex h-screen">
-
         <div className="flex-1">
           <HomeCard />
         </div>
