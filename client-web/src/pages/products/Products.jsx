276f67326bdf4299b92340e1d11a97458500c2e9
mokautsar30 <mohammadkautsar30@gmail.com>
2024-01-20 19:45:37 +0700

    feat: populated product management

--- client-web/src/pages/products/Products.jsx
+++ client-web/src/pages/products/Products.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import "./Products.scss";
 import { products } from "../../data";
 import Table from "../../components/table/Table";
-
+import axios from "axios";
+import { useEffect } from "react";
 
 const formatPriceToRupiah = (price) => {
   return new Intl.NumberFormat("id-ID", {
@@ -12,73 +13,73 @@ const formatPriceToRupiah = (price) => {
   }).format(price);
 };
 
-
-const columns = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "image",
-    headerName: "Image",
-    width: 100,
-    renderCell: (params) => {
-      return React.createElement("img", { src: params.row.image || "/noavatar.png", alt: "" });
-    },
-  },
-  {
-    field: "name",
-    headerName: "name",
-    width: 250,
-  },
-  {
-    field: "stock",
-    headerName: "stock",
-    width: 150,
-  },
-  {
-    field: "price",
-    headerName: "Price",
-    width: 200,
-    renderCell: (params) => {
-      return formatPriceToRupiah(params.value);
-    },
-  },
-  {
-    field: "discQty",
-    headerName: "discQty",
-    width: 200,
-  },
-  {
-    field: "discPercent",
-    headerName: "discPercent",
-    width: 200,
-  },
-  {
-    field: "category",
-    headerName: "category",
-    width: 200,
-  },
-  {
-    field: "isAvailable",
-    headerName: "Availability",
-    width: 150,
-    renderCell: (params) => {
-      return params.value ? "In Stock" : "Out of Stock";
-    },
-  },
-];
-
 const Products = () => {
   const [open, setOpen] = useState(false);
+  const [listProducts, setListProducts] = useState([]);
 
-  return (
-    React.createElement("div", { className: "products" },
-      React.createElement("div", { className: "info" },
-        React.createElement("h1", null, "Products"),
-        React.createElement("button", { onClick: () => setOpen(true) }, "Add New Products")
-      ),
-      React.createElement(Table, { slug: "products", columns: columns, rows: products }),
-      // {open && React.createElement(Add, { slug: "product", columns: columns, setOpen: setOpen })}
-    )
-  );
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios({
+        method: "GET",
+        url: "http://localhost:3000/products"
+      });
+      console.log("Data API:", data);
+      // data pada apinya nested
+      setListProducts(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const displayedKeys = ["id", "image", "name", "category", "stock", "price", "discQty", "discQty", "isAvailable"];
+
+  const columns =
+  listProducts && listProducts.length > 0
+    ? displayedKeys.map((key) => ({
+        field: key,
+        headerName: key.charAt(0).toUpperCase() + key.slice(1),
+        width: 160,
+        renderCell: (params) => {
+          return key === "price"
+            ? formatPriceToRupiah(params.value)
+            : params.value;
+        },
+      }))
+    : [];
+
+    const filteredListProducts =
+    listProducts &&
+    listProducts.map((product, index) =>
+      displayedKeys.reduce((obj, key) => {
+        if (key === "id") {
+          obj[key] = index + 1;
+        } else if (key === "isAvailable") {
+          obj[key] = product[key] ? "In Stock" : "Out of Stock";
+        } else {
+          obj[key] = product[key];
+        }
+        return obj;
+      }, {})
+    );
+
+    return (
+      <div className="products">
+        <div className="info">
+          <h1>Products</h1>
+          <button onClick={() => setOpen(true)}>Add New Products</button>
+        </div>
+        <Table slug="products" columns={columns} rows={filteredListProducts} />
+        {/* Add your modal or form component here */}
+        {/* {open && <Add slug="product" columns={columns} setOpen={setOpen} />} */}
+      </div>
+    );
 };
 
 export default Products;
+
+
