2e4274d4609abfe346018239ced9b0bb078b7f58
AlvinLiem <avh8project2023@gmail.com>
2024-01-23 17:39:36 +0700

    feat: wiring dashboard top stores

--- client-web/src/components/topStore/TopStore.jsx
+++ client-web/src/components/topStore/TopStore.jsx
@@ -1,26 +1,44 @@
-import { topStore } from "../../data"
-import "./topStore.scss"
+import { useEffect, useState } from "react";
+import "./topStore.scss";
+import axios from "axios";
 
 const TopStore = () => {
+  const [topStore, setTopStore] = useState([]);
+
+  async function fetchData() {
+    const { data } = await axios({
+      method: "GET",
+      url: import.meta.env.VITE_BASE_URL + "/stores",
+      headers: {
+        "ngrok-skip-browser-warning": "69420",
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
+    });
+    setTopStore(data);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="topBox">
       <h1>Top Store</h1>
       <div className="list">
-        {topStore.map(store=>(
-          <div className="listItem" key={store.id}>
-            <div className="store">
-              <img src={store.img} alt="" />
-              <div className="storeText">
-                <span className="name">{store.name}</span>
-                <span className="ownerName">{store.ownerName}</span>
+        {topStore &&
+          topStore.slice(0, 7).map((store) => (
+            <div className="listItem" key={store._id}>
+              <div className="store">
+                <img src={store.photo} alt="" />
+                <div className="storeText">
+                  <span className="name">{store.name}</span>
+                  {/* <span className="ownerName">{store.ownerName}</span> */}
+                </div>
               </div>
+              <span className="totalOrder">Rp.{store.confirmedOrderValue}</span>
             </div>
-            <span className="totalOrder">Rp.{store.totalOrder}</span>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopStore
+export default TopStore;
