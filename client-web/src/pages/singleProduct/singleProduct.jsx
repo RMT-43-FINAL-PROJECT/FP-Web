13b35e44703f9069cea95f3464bf712e55f3ae30
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2024-01-24 03:43:31 +0700

    feat: added update product but not fixed

--- client-web/src/pages/singleProduct/singleProduct.jsx
+++ client-web/src/pages/singleProduct/singleProduct.jsx
@@ -23,7 +23,7 @@ const SingleProduct = () => {
           Authorization: "Bearer " + localStorage.getItem("access_token"),
         },
       });
-      console.log("Data API:", data);
+      // console.log("Data API:", data);
       // data pada apinya nested
       setListProducts(data);
     } catch (error) {
