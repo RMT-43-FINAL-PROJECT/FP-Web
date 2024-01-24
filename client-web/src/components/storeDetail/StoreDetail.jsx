e20f3b3f3ffda8fb1372a3a16a0aa1515e730009
mokautsar30 <mohammadkautsar30@gmail.com>
2024-01-24 12:58:13 +0700

    feat: stores-layout-detail

--- client-web/src/components/storeDetail/StoreDetail.jsx
+++ client-web/src/components/storeDetail/StoreDetail.jsx
@@ -5,6 +5,19 @@ const StoreDetail = ({ listStore }) => {
   const getStatus = () => {
     return listStore.status ? "Verified" : "unverified";
   };
+  const formattedTime = new Date(listStore?.createdAt).toLocaleString(
+    "en-US",
+    {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZoneName: "short",
+    }
+  );
 
   const renderCoordinates = () => {
     if (listStore.location && listStore.location.coordinates) {
@@ -24,66 +37,51 @@ const StoreDetail = ({ listStore }) => {
     return null;
   };
   return (
-    <div className="products">
-      <div className="">
-        <div className="info">
-          <div className="container px-6 py-10 mx-auto">
-            <div className="mt-8 lg:flex lg:items-center">
-              <img
-                className="h-65 lg:w-1/2 lg:h-70 rounded-xl mb-6 lg:mb-0"
-                src={listStore.photo}
-                alt=""
-              />
+<>
+        <div class="card">
+          <div class="card__title">
+            <div class="icon">
+              <a href="#">
+                <i class="fa fa-arrow-left"></i>
+              </a>
+            </div>
+            <h3> Store Detail</h3>
+          </div>
+          <div class="card__body">
+            <div class="half">
+              <div class="featured_text">
+                <h1> {listStore.name}</h1>
+                <p class="sub"> {listStore.ownerName}</p>
+              </div>
+              <div class="imageStore">
+                <img src={listStore.photo} alt={listStore.name} />
+              </div>
+              <span class="stock">
+                <span> Status : {getStatus()}</span>
+              </span>
+            </div>
+            <div class="half">
+              <div class="description">
+                <p></p>
+              </div>
 
-              <div className="lg:w-1/2 lg:ml-6">
-                <p className="text-lg text-black uppercase underline font-bold">
-                  Store Detail
-                </p>
-
-                <a
-                  href="#"
-                  className="block mt-4 text-3xl font-semibold text-gray-800 hover:underline dark:text-black md:text-4xl"
-                >
-                  {listStore.name}
-                </a>
-
-                <div className="card-description mt-3 rounded-md">
-                  <p className="text-lg text-gray-700 dark:text-black md:text-2xl underline">
-                    {listStore.ownerName}
-                  </p>
-                  <h1 className="text-lg text-gray-700 dark:text-black font-bold">
-                    {getStatus()}
-                  </h1>
-                </div>
-                <br />
-                <div>
-                  <h1 className="text-lg text-gray-700 dark:text-black font-bold">
-                    phone: {listStore.mobilePhone}
-                  </h1>
-                </div>
-                <div>
-                  <h1 className="text-lg text-gray-700 dark:text-black font-bold">
-                    address: {listStore.address}
-                  </h1>
-                </div>
-                <div>
-                  <h1 className="text-lg text-gray-700 dark:text-black font-bold">
-                    Since: {listStore.joinDate}
-                  </h1>
-                </div>
-
-                <a
-                  href="#"
-                  className="inline-block mt-2 text-blue-500 underline hover:text-blue-400 text-lg"
-                >
-                  update
-                </a>
+              <div class="reviews">
+                <span></span>
               </div>
             </div>
           </div>
+          <div class="card__footer">
+            <div class="recommend">
+              <h3> phone: {listStore.mobilePhone}</h3>
+              <h3> Since: {formattedTime}</h3>
+              <p> address: {listStore.address}</p>
+            </div>
+            <div class="action">
+            <button onClick={() => setOpen(true)}>Edit Status</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+        </>
   );
 };
 
